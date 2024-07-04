4296f9341df260dca5838498b64f3ad042311a07
iambeno1 <benonygabriel40@gmail.com>
2024-07-04 23:18:02 +0700

    belajar utility function

--- phpdasar/pertemuan04/utility.php
+++ phpdasar/pertemuan04/utility.php
@@ -49,6 +49,34 @@
     }
 
     ?>
+
+
+    <br><br>
+    <h3>sleep()</h3>
+    <p>Fungsi sleep() digunakan untuk menunda (menghentikan) eksekusi skrip selama jumlah detik tertentu yang ditentukan sebelum melanjutkan eksekusi skrip berikutnya.
+        Berguna untuk simulasi penundaan atau untuk mengontrol alur eksekusi dalam situasi tertentu, misalnya dalam skenario yang melibatkan pengulangan atau pengaturan interval waktu tertentu.</p>
+    <b>Contoh</b>
+    <?php
+    echo "Memulai proses...\n";
+    sleep(3); // Menunda eksekusi selama 3 detik
+    echo "Proses selesai.";
+    ?>
+
+    <br><br>
+    <h3>die()</h3>
+    <p>Fungsi die() digunakan untuk mengakhiri eksekusi skrip PHP secara paksa dan segera menghentikan proses eksekusi. Biasanya digunakan untuk menampilkan pesan kesalahan atau pesan khusus sebelum menghentikan skrip.
+        Fungsi exit() juga memiliki fungsi yang sama dengan die() dan bisa digunakan secara bergantian dengan die().
+    </p>
+    <b>Contoh</b>
+    <?php
+    $data = null;
+    if (!$data) {
+        die("Data tidak tersedia"); // Menampilkan pesan dan menghentikan skrip
+    }
+    // Code setelah ini tidak akan dieksekusi jika kondisi di atas terpenuhi
+
+    ?>
+
 </body>
 
 </html>
