3abdce64e8f254f050afc93dac8d714c24c4e7fc
iambeno1 <benonygabriel40@gmail.com>
2024-08-01 20:40:17 +0700

    belaljar membuat login form

---
+++ phpdasar/pertemuan15/login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+
+<body>
+    <h1>Halaman Login</h1>
+    <form action="" method="post">
+        <ul>
+            <li>
+                <label for="username">Username : </label>
+                <input type="text" name="username" id="username" placeholder="Masukkan username kamu" required>
+            </li>
+            <li>
+                <label for="password">Password : </label>
+                <input type="password" name="password" id="password" placeholder="Masukkan password kamu" required>
+            </li>
+        </ul>
+    </form>
+</body>
+
+</html>
