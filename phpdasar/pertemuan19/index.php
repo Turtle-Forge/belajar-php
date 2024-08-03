48b15c6682750028cc1a0c0e1a78a0e96bbfc8f7
iambeno1 <benonygabriel40@gmail.com>
2024-08-03 10:57:09 +0700

    membuat live search dengan ajax

--- phpdasar/pertemuan19/index.php
+++ phpdasar/pertemuan19/index.php
@@ -39,42 +39,48 @@ if (isset($_POST["cari"])) {
     <a href="tambah.php">Tambah data</a>
     <br><br>
     <form action="" method="post">
-        <input type="text" name="keyword" placeholder="Masukkan kata kunci..." autofocus>
-        <button name="cari" type="submit">Cari</button>
+        <input type="text" name="keyword" placeholder="Masukkan kata kunci..." autofocus id="keyword">
+        <button name="cari" type="submit" id="tombol-cari">Cari</button>
         <button name="reset" type="submit">Reset</button>
     </form>
     <br><br>
 
 
-    <table border="1" cellpadding="10" cellspacing="0">
-        <tr>
-            <th>No.</th>
-            <th>Title</th>
-            <th>Author</th>
-            <th>Tahun terbit</th>
-            <th>Cover</th>
-            <th>Aksi</th>
-        </tr>
-
-        <?php $id = 1; ?>
-        <?php foreach ($books as $book) : ?>
+    <div id="container">
+        <table border="1" cellpadding="10" cellspacing="0">
             <tr>
-                <td><?= $id ?></td>
-                <td><?= $book["title"] ?></td>
-                <td><?= $book["author"] ?></td>
-                <td><?= $book["tahunTerbitt"] ?></td>
-                <td>
-                    <img src="img/<?= $book["cover"] ?>" alt="img" height="80px">
-                </td>
-                <td>
-                    <a href="ubah.php?id=<?= $book['id'] ?>">ubah |</a>
-                    <a href="hapus.php?id=<?= $book['id'] ?>" onclick="return confirm('Ingin menghapus data?')">hapus</a>
-                </td>
+                <th>No.</th>
+                <th>Title</th>
+                <th>Author</th>
+                <th>Tahun terbit</th>
+                <th>Cover</th>
+                <th>Aksi</th>
             </tr>
-            <?php $id++ ?>
-        <?php endforeach; ?>
 
-    </table>
+            <?php $id = 1; ?>
+            <?php foreach ($books as $book) : ?>
+                <tr>
+                    <td><?= $id ?></td>
+                    <td><?= $book["title"] ?></td>
+                    <td><?= $book["author"] ?></td>
+                    <td><?= $book["tahunTerbitt"] ?></td>
+                    <td>
+                        <img src="img/<?= $book["cover"] ?>" alt="img" height="80px">
+                    </td>
+                    <td>
+                        <a href="ubah.php?id=<?= $book['id'] ?>">ubah |</a>
+                        <a href="hapus.php?id=<?= $book['id'] ?>" onclick="return confirm('Ingin menghapus data?')">hapus</a>
+                    </td>
+                </tr>
+                <?php $id++ ?>
+            <?php endforeach; ?>
+
+        </table>
+    </div>
+
+
+    <!-- JS -->
+    <script src="js/script.js"></script>
 </body>
 
 </html>
