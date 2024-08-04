847be81931eb48f4a912e322707afaaacabc8067
iambeno1 <benonygabriel40@gmail.com>
2024-08-04 14:14:58 +0700

    belajar class dan object

---
+++ phpoop/pertemuan1/coba.php
@@ -0,0 +1,15 @@
+<?php
+
+// Membuat kelas sama dengan java
+class Coba
+{
+}
+
+// membuat objek dari kelas Coba
+$a = new Coba();
+$b = new Coba();
+$c = new Coba();
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
---
+++ phpoop/pertemuan1/toko.php
@@ -0,0 +1,11 @@
+<?php
+
+
+// Toko yang menjual komik dan game
+class Toko
+{
+}
+
+$produk1 = new Toko();
+$produk2 = new Toko();
+$produk3 = new Toko();
