2acfe6a61bee59a315fbd52d75cd6a37747494af
iambeno1 <benonygabriel40@gmail.com>
2024-08-19 22:33:18 +0700

    belajar membuat assets

--- mvc/app/views/about/index.php
+++ mvc/app/views/about/index.php
@@ -1,2 +1,9 @@
-    <h1>About me</h1>
-    <p>Halo nama saya <?= $data["nama"] ?>, saya adalah seorang <?= $data["pekerjaan"] ?>.</p>
+    <div class="container">
+        <div class="jumbotron jumbotron-fluid mt-5">
+            <div class="container">
+                <h1 class="display-4">About me</h1>
+                <img src="<?= BASEURL ?>/img/profil.jpeg" alt="Beno" width="200">
+                <p class="lead mt-3">Halo nama saya <?= $data["nama"] ?>, saya adalah seorang <?= $data["pekerjaan"] ?>.</p> <a href="<?= BASEURL ?>/about/page">My pages</a>
+            </div>
+        </div>
+    </div>
--- mvc/app/views/about/page.php
+++ mvc/app/views/about/page.php
@@ -1,1 +1,10 @@
-<h1>My pages</h1>
+<div class="container">
+    <div class="jumbotron jumbotron-fluid mt-5">
+        <div class="container">
+            <a href="<?= BASEURL ?>/about">Back</a>
+            <h1 class="display-4 mt-5">My pages</h1>
+            <p class="lead">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum cumque ducimus voluptas debitis iste, quasi quidem commodi dolorum odio quam minima culpa natus! Harum voluptatem, ducimus omnis doloribus alias similique.</p>
+        </div>
+    </div>
+
+</div>
