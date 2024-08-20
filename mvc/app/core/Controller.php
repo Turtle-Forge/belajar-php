9809dc20dc01092720fe07169db445e8c869a39f
iambeno1 <benonygabriel40@gmail.com>
2024-08-20 22:46:46 +0700

    belajar membuat model'

--- mvc/app/core/Controller.php
+++ mvc/app/core/Controller.php
@@ -2,8 +2,17 @@
 
 class Controller
 {
+    // ini controller untuk mengelola view
     public function view($view, $data = [])
     {
         require_once "../app/views/" . $view . ".php";
+    }
+
+
+    // ini controller untuk mengelola model
+    public function model($model)
+    {
+        require_once "../app/models/" . $model . ".php";
+        return new $model;
     }
 }
---
+++ mvc/app/models/Mahasiswa_model.php
@@ -0,0 +1,49 @@
+<?php
+
+class Mahasiswa_model
+{
+    // private $mhs = [
+    //     [
+    //         "nama" => "Benony Gabriel",
+    //         "nim" => "105222002",
+    //         "jurusan" => "Ilmu Komputer",
+    //         "angkatan" => 2022
+    //     ],
+    //     [
+    //         "nama" => "Ridho Pratama",
+    //         "nim" => "105222022",
+    //         "jurusan" => "Ilmu Kimia",
+    //         "angkatan" => 2021
+    //     ],
+    //     [
+    //         "nama" => "Jhon Doe",
+    //         "nim" => "105222032",
+    //         "jurusan" => "Teknik Elektro",
+    //         "angkatan" => 2020
+    //     ]
+    // ];
+
+
+    // melakukan konek database dengan cara PDO. tidak menggunakan mysqli query
+    private $dbh; // database handler
+    private $statement;
+
+    public function __construct()
+    {
+        // data source name
+        $dsn = "mysql:host=localhost;dbname=phpmvc";
+        try {
+            $this->dbh = new PDO($dsn, "root", "");
+        } catch (PDOException $e) {
+            die($e->getMessage());
+        }
+    }
+
+
+    public function getAllMahasiswa()
+    {
+        $this->statement = $this->dbh->prepare("SELECT * FROM mahasiswa");
+        $this->statement->execute();
+        return $this->statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
